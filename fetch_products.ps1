aabf086e6ecc1300444ff37cf862e2ad29cef227
Braspump <contato@braspump.com.br>
2026-05-13 12:45:04 -0300

    refactor: remove legacy product fetching and categorization PowerShell scripts

--- fetch_products.ps1
+++
@@ -1,4 +0,0 @@
-$creds = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("luis@braspump.com.br:aaOc 5QPj ocIG ni5X XrF0 dqiw"))
-$headers = @{Authorization = "Basic $creds"}
-$res = Invoke-RestMethod -Uri "https://equipobras.com.br/wp-json/wc/v3/products" -Headers $headers
-$res | Select-Object id, name | Format-Table -AutoSize
