60ca52272cc322283c875fff3876bf431f96483f
Braspump <contato@braspump.com.br>
2026-05-13 16:04:17 -0300

    feat: add PowerShell scripts for fetching product variables and media, along with corresponding JSON data files

---
+++ fetch_vars.ps1
@@ -0,0 +1,4 @@
+$auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("luis@braspump.com.br:aaOc 5QPj ocIG ni5X XrF0 dqiw"))
+$headers = @{ Authorization = "Basic $auth" }
+$variations = Invoke-RestMethod -Uri "https://equipobras.com.br/wp-json/wc/v3/products/38/variations" -Headers $headers
+$variations | ConvertTo-Json -Depth 5 | Out-File "C:\Users\luis\Desktop\site braspump\prod_38_vars.json" -Encoding UTF8
